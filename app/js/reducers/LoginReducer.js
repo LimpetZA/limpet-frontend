e0a5bb73d9e325ac453fe7cdc35deddc982053a0
St John Giddy <stjohngiddy@gmail.com>
2018-02-01 16:10:42 +0200

    feat(Login): redux support for login

--- app/js/reducers/LoginReducer.js
+++ app/js/reducers/LoginReducer.js
@@ -1,11 +1,13 @@
 'use strict'
 
+const cookies = new (require('universal-cookie'))()
+
 export default function LoginReducer(state = {}, action) {
   // Do nothing
   const { type, payload } = action
   switch(type) {
     case "ATTEMPT_LOGIN": {
-      const { res, uri } = payload
+      const { res } = payload
       let { isLoggedIn, attemptedLogin } = state
       if(res.status === 200) {
         isLoggedIn = true
@@ -18,6 +20,14 @@ export default function LoginReducer(state = {}, action) {
     }
     case "RESET_ATTEMPT":
       return { ...state, attemptedLogin : false}
+    case "USER_REGISTER":
+    //console.log("here ", payload)
+      //const { res: { json } } = payload
+      //const { success } = json
+      const { res: { status } } = payload
+      //console.log(payload.res.json().then(console.log))
+      if(status === 200) return { ...state, registerSuccess: true }
+      else return { ...state, registerSuccess: false}
   }
   return state
 }
